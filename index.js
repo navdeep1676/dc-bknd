88d30767fd7affe28794091ecd8d8942c8e6e70c
navdeep1676 <navdeepdahiya753@gmail.com>
2023-03-06 18:37:32 +0000

    udpates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose")
 const connectDb = async () => {
-console.log("I  gdfg dfran gfdg fd")
+console.log("I gdg  gdfg dfran gfdg fd")
   try {
     await mongoose.connect('mongodb://navdeep:monu1676@127.0.0.1:27017/dc')
     console.log("conneected db successfully")
