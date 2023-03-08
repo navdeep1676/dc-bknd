a4b43c133ad8c0b6153a701755f128c30b7fe543
navdeep <navdeep.dahiya@sundaymobility.com>
2023-03-08 20:29:03 +0530

    updates

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const connectDb = async () => {
-console.log("I gdg  gdfg dfran gfdg fd")
+  console.log("I gdg  gdfg dfran gfdg fd");
   try {
-    await mongoose.connect('mongodb://navdeep:monu1676@127.0.0.1:27017/dc')
-    console.log("conneected db successfully")
+    await mongoose.connect("mongodb://navdeep:monu1676@127.0.0.1:27017/dc");
+    console.log("conneected db successfully");
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-}
-connectDb()
-app.get("/",(req,res)=>{
-res.send("welcome to api pipeline")
-})
-app.listen(4000,()=>{
-console.log("Server is Running 5000")
-})
+};
+connectDb();
+app.get("/", (req, res) => {
+  res.send("welcome to api pipeline");
+});
+app.listen(4000, () => {
+  console.log("Server is Running on PORT 5000");
+});
