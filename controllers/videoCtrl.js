b8fafbf27c6f915a49a43fa82e9af376947e54d4
Navdeep <navdeepdahiya753@gmail.com>
2023-03-12 00:29:17 +0530

    updates

--- controllers/videoCtrl.js
+++ controllers/videoCtrl.js
@@ -24,7 +24,6 @@ const postVideo = async (req, res) => {
       code: "VIDEO_FAILED",
       msg: "Something went wrong! Please contact the site administrator.",
       error: error,
-      newerror: newpath,
     });
   }
 };
